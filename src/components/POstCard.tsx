8ef993578d305919fdae643f5343e914390b9690
Aayush-Pandey05 <aayushpandeydsce23@gmail.com>
2025-05-04 13:41:18 +0530

    solved new

--- src/components/POstCard.tsx
+++ src/components/POstCard.tsx
@@ -191,7 +191,7 @@ function POstCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
             <div className="space-y-4 pt-4 border-t">
               <div className="space-y-4">
                 {/* DISPLAY COMMENTS */}
-                {post.comments.map((comment) => (
+                {post.comments.map((comment: any) => (
                   <div key={comment.id} className="flex space-x-3">
                     <Avatar className="size-8 flex-shrink-0">
                       <AvatarImage
