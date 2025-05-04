55799bda5fa4dd81665d939470aef97649ed8b16
Aayush-Pandey05 <aayushpandeydsce23@gmail.com>
2025-05-04 14:49:15 +0530

    New solve

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,14 +3,16 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || 
-  new PrismaClient({
+const prismaClientSingleton = () => {
+  return new PrismaClient({
     datasources: {
       db: {
-        url: "postgresql://neondb_owner:npg_LDrOg5Yjl3RJ@ep-proud-feather-a5cnqhw4-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+        url: process.env.DATABASE_URL
       }
     }
   });
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
